ad7d52b2b379eb7048759b6287444c6cc00083d0
yui <ga.2.y1220@gmail.com>
2021-05-16 10:13:38 +0200

    add, today, random function, terminal operation

---
+++ ngs/screenshots/today_add_random/today_add_random.js
@@ -0,0 +1,31 @@
+Math.random()
+3 == '3'
+function today(){}
+function today(){
+return new Date();
+}
+today()
+function add(x,y){
+  return x= y;
+}
+
+function random(){
+  return Math.random(); 
+ }
+ 
+function add(x,y){
+  return x= y;
+}
+
+function random(){
+  return Math.random();
+}
+
+random()
+add(3,4)
+function add(){
+}
+function add(x,y){
+return x+y;
+}
+add(6,7)
